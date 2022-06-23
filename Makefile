9fb3a41287845bbb8e1e4bb6b560501a2aa394ed
roaddust2 <roaddust2@yahoo.com>
2022-06-23 22:22:58 +0400

    Fix linter errors, add .vscode/ to .gitignore

--- Makefile
+++ Makefile
@@ -16,3 +16,6 @@ package-reinstall:
 
 brain-games:
 	poetry run brain-games
+
+lint:
+	poetry run flake8 brain_games
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 from brain_games.cli import welcome_user
 
+
 def main():
-	print("Welcome to the Brain Games!")
-	welcome_user()
-	return
+    print("Welcome to the Brain Games!")
+    welcome_user()
+    return
+
 
 if __name__ == '__main__':
-	main()
+    main()
