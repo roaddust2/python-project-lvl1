e11078eace92a25965f0dbf18346576d9f0276fa
roaddust2 <roaddust2@yahoo.com>
2022-07-19 14:15:24 +0400

    Small fix

--- brain_games/games/brain_gcd_engine.py
+++ brain_games/games/brain_gcd_engine.py
@@ -6,7 +6,7 @@ RULE = 'Find the greatest common divisor of given numbers.'
 
 def find_gcd(num_1, num_2):
     if num_1 < num_2:
-        num_1, num_2 = num_2,num_1
+        num_1, num_2 = num_2, num_1
     while num_2 != 0:
         num_1, num_2 = num_2, num_1 % num_2
     return num_1
